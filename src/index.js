1139be02611f25c61b73e1a0534ba25ece89ad6a
forl <forl.yang@gmail.com>
2018-12-13 23:53:43 +0800

    add comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import createAll from './createAll'
 import plainStructure from './structure/plain'
 
+/**
+ * 默认以 plain object 的形式维护 store
+ */
 export const {
   LOCATION_CHANGE,
   CALL_HISTORY_METHOD,
